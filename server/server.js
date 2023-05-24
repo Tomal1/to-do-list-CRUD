24174afd9ee31cf3a20c2b96f0cf77a87e2911a9
Minhazul Islam <minhazulislam@hotmail.com>
2023-05-24 14:24:56 +0100

    app has been connected to the database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3001; 
 
+
+app.use(express.urlencoded({ extended: true }));
+
+// express by default dose not use json, this allow it
+app.use(express.json());
+
+const db = require("./config/connection");
+
 app.use(express.static('public'));
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`listening to port ${PORT}`)
 })
