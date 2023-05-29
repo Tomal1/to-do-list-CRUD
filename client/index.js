084fe8967b571b2443da40718f4605b2cf275ed5
Minhazul Islam <minhazulislam@hotmail.com>
2023-05-29 17:20:34 +0100

    post fetch method added in index.js and cors libary installed on server.js

--- client/index.js
+++ client/index.js
@@ -4,18 +4,29 @@ const inputField = document.querySelector("#inputField")
 
 addToDo.addEventListener("click", ()=>{
 
+    console.log(inputField.value) //displays what you are typing in to the input field
+
+fetch("http://localhost:3001/post",{
+    method: "POST",
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify({toDos: inputField.value})
+})
+.then(res => res.json())
+.then(data => console.log(data))
+
     const paragraph = document.createElement("p");
     paragraph.innerText =  inputField.value;
     paragraph.classList.add("paragraph-styling");
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
 
-    paragraph.addEventListener("click", ()=>{
-        paragraph.style.textDecoration = "line-through";
-    })
+    paragraph.addEventListener("click", ()=> {
+        paragraph.innerText
+    });
 
     paragraph.addEventListener("dblclick", ()=>{
-        toDoContainer.removeChild(paragraph);;
+        toDoContainer.removeChild(paragraph);
     })
-
 })
+
+
--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@ const express = require("express");
 
 //npm i dotenv
 require("dotenv").config();
+//npm i cors (gives permision for front end to access backend)
+const cors = require("cors");
 
 const app = express();
-const PORT = 3001; 
+const PORT = 3001 || process.env.PORT; 
+
+
+app.use(cors({ // this is needed in express server to give permission
+    origin: "http://127.0.0.1:5501"
+}));
 
 
 app.use(express.urlencoded({ extended: true }));
-
 // express by default dose not use json, this allow it
 app.use(express.json());
 
@@ -29,7 +35,7 @@ app.get("/",(req,res)=>{
     })
 })
 
-app.post("/",(req,res)=>{
+app.post("/post",(req,res)=>{
 
     const sql = "INSERT INTO input (toDos) VALUE (?)";
     const values = [req.body.toDos];
@@ -43,8 +49,7 @@ app.post("/",(req,res)=>{
     })
 })
 
-
-app.delete("/", (req, res)=>{
+app.delete("/delete", (req, res)=>{
     const values = [req.body.id]
 
     const sql =  `DELETE FROM input WHERE id = ?`; // Always use backticks when not hard coding
@@ -54,13 +59,13 @@ app.delete("/", (req, res)=>{
             throw err;
         } else{
             console.log(data)
-            return res.json("deleted user successfully");
+            return res.json("deleted the todo successfully");
         }
     })
 });
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(/*process.env.PORT ||*/ PORT, () => {
     console.log(`listening to port ${PORT}`)
 })
