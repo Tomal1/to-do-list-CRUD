20539a14c719248328d381805f30b3434c6ea161
Minhazul Islam <minhazulislam@hotmail.com>
2023-05-29 18:58:28 +0100

    all files formated for cleaner code style

--- client/index.js
+++ client/index.js
@@ -1,55 +1,51 @@
 const addToDo = document.querySelector("#addToDo");
-const toDoContainer = document.querySelector("#toDoContainer")
-const inputField = document.querySelector("#inputField")
+const toDoContainer = document.querySelector("#toDoContainer");
+const inputField = document.querySelector("#inputField");
 
 const start = () => {
-    fetch("http://localhost:3001")
-    .then(res => res.json())
-    .then(data => {
-        
-        console.log(data)
+  fetch("http://localhost:3001")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
 
-        for(let i=0; i<data.length; i++){
-            const paragraph = document.createElement("p");
-            paragraph.innerText =  data[i].toDos;
-            paragraph.classList.add("paragraph-styling");
-            toDoContainer.appendChild(paragraph);
+      for (let i = 0; i < data.length; i++) {
+        const paragraph = document.createElement("p");
+        paragraph.innerText = data[i].toDos;
+        paragraph.classList.add("paragraph-styling");
+        toDoContainer.appendChild(paragraph);
 
-            paragraph.addEventListener("dblclick", () =>{
-                let idFinder = "";
-                if(paragraph.innerText === data[i].toDos){
-                    idFinder = idFinder + data[i].id;
-                }
+        paragraph.addEventListener("dblclick", () => {
+          let idFinder = "";
+          if (paragraph.innerText === data[i].toDos) {
+            idFinder = idFinder + data[i].id;
+          }
 
-                console.log(idFinder);
+          console.log(idFinder);
 
-                    fetch("http://localhost:3001/delete", {
-                        method: "DELETE",
-                        headers: {"Content-type": "application/json"},
-                        body: JSON.stringify({
-                            id: idFinder
-                        })
-                    })
-                    .then(res => res.json())
-                    .then(data => console.log(data))
-            })
-        }
-        inputField.value = "";
-    })
-} 
-start()
+          fetch("http://localhost:3001/delete", {
+            method: "DELETE",
+            headers: { "Content-type": "application/json" },
+            body: JSON.stringify({
+              id: idFinder,
+            }),
+          })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+        });
+      }
+      inputField.value = "";
+    });
+};
+start();
 
+addToDo.addEventListener("click", () => {
+  console.log(inputField.value); //displays what you are typing in to the input field
 
-addToDo.addEventListener("click", ()=>{
-    console.log(inputField.value) //displays what you are typing in to the input field
-
-fetch("http://localhost:3001/post",{
+  fetch("http://localhost:3001/post", {
     method: "POST",
-    headers: {"Content-type": "application/json"},
-    body: JSON.stringify({toDos: inputField.value})
-})
-.then(res => res.json())
-.then(console.log("posted successfully"))
-})
-
-
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ toDos: inputField.value }),
+  })
+    .then((res) => res.json())
+    .then(console.log("posted successfully"));
+});
--- server/server.js
+++ server/server.js
@@ -7,13 +7,14 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-const PORT = 3001 || process.env.PORT; 
+const PORT = 3001 || process.env.PORT;
 
-
-app.use(cors({ // this is needed in express server to give permission
-    origin: "http://127.0.0.1:5501"
-}));
-
+app.use(
+  cors({
+    // this is needed in express server to give permission
+    origin: "http://127.0.0.1:5501",
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 // express by default dose not use json, this allow it
@@ -21,51 +22,48 @@ app.use(express.json());
 
 const db = require("./config/connection");
 
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM input";
 
-app.get("/",(req,res)=>{
-
-    const sql = "SELECT * FROM input";
-
-    db.query(sql, (err, data) =>{
-        if(err){
-            throw err;
-        }else{
-            return res.json(data);   
-        }
-    })
-})
-
-app.post("/post",(req,res)=>{
-
-    const sql = "INSERT INTO input (toDos) VALUE (?)";
-    const values = [req.body.toDos];
-
-    db.query(sql, values, (err, data) =>{
-        if(err){
-            throw err;
-        }else{
-            return res.json(data);   
-        }
-    })
-})
-
-app.delete("/delete", (req, res)=>{
-    const values = [req.body.id]
-
-    const sql =  `DELETE FROM input WHERE id = ?`; // Always use backticks when not hard coding
-    
-    db.query(sql,values,(err,data) => {
-        if(err){
-            throw err;
-        } else{
-            console.log(data)
-            return res.json("deleted the todo successfully");
-        }
-    })
+  db.query(sql, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
+  });
 });
 
-app.use(express.static('public'));
+app.post("/post", (req, res) => {
+  const sql = "INSERT INTO input (toDos) VALUE (?)";
+  const values = [req.body.toDos];
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+app.delete("/delete", (req, res) => {
+  const values = [req.body.id];
+
+  const sql = `DELETE FROM input WHERE id = ?`; // Always use backticks when not hard coding
+
+  db.query(sql, values, (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(data);
+      return res.json("deleted the todo successfully");
+    }
+  });
+});
+
+app.use(express.static("public"));
 
 app.listen(/*process.env.PORT ||*/ PORT, () => {
-    console.log(`listening to port ${PORT}`)
-})
+  console.log(`listening to port ${PORT}`);
+});
